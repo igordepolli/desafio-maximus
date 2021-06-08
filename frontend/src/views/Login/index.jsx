9317884603d95246396d43a5ba36155cf9c52f57
Igor Depolli <igorcler@gmail.com>
2021-06-08 13:07:04 -0300

    Start implementation form

--- frontend/src/views/Login/index.jsx
+++ frontend/src/views/Login/index.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { SignIn } from '../../components';
+import { Grid, Paper, Typography } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import FormLogin from '../../components/FormLogin';
+import './styles.css';
 
 const Login = () => {
   return (
-    <div className="App">
-      <SignIn />
+    <div className="container">
+      <Paper elevation={10}>
+        <Grid alignItems="center" justify="center">
+          <Grid align="center" item>
+            <Typography variant="h1" align="center">
+              Log In
+            </Typography>
+          </Grid>
+          <Grid item>
+            <FormLogin />
+          </Grid>
+          <Grid align="center" item>
+            <Link href="/" underline="always">
+              Novo usuário? Registre-se.
+            </Link>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 };
